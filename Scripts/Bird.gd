e863ac184b2235ec22f4858d52ef25fa36c56b46
Dizo <jbr1989@gmail.com>
2025-10-28 20:30:18 +0100

    FIX: Bird touch

--- Scripts/Bird.gd
+++ Scripts/Bird.gd
@@ -4,36 +4,20 @@ extends Area2D
 export(int) var impulse_strength = 75
 export(int) var horizontal_speed = 50
 export(int) var speed_increment = 5
-export(int) var gravity = 980
 
 var alive = true
 var velocity = Vector2()
-
-# Input buffering
-var input_buffer = false
-var buffer_time = 0.1
-var buffer_timer = 0.0
 
 func _ready():
 	# Conectar señales de colisión
 	connect("body_entered", self, "_on_body_entered")
 	connect("area_entered", self, "_on_area_entered")
-	
-	# Configurar input
-	set_process_input(true)
 
 func getZoom():
 	return $Camera2D.zoom.y
 
 func _physics_process(delta):
-	if not alive:
-		return
-	
-	# Procesar input buffer
-	if input_buffer:
-		buffer_timer -= delta
-		if buffer_timer <= 0:
-			input_buffer = false
+	if not alive: pass
 
 	# Movimiento horizontal constante
 	velocity.x = horizontal_speed
@@ -84,51 +68,3 @@ func die():
 
 func _set_animation(pos):
 	$AnimatedSprite.animation=pos
-
-# ✅ SISTEMA DE INPUT MEJORADO
-func _input(event):
-	if not alive:
-		return
-	
-	# Detectar diferentes tipos de input
-	var should_jump = false
-	
-	# Touch input (móviles)
-	if event is InputEventScreenTouch and event.pressed:
-		should_jump = true
-		
-	# Mouse input (PC)
-	elif event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
-		should_jump = true
-		
-	# Keyboard input (PC)
-	elif event is InputEventKey and event.pressed:
-		if event.scancode == KEY_SPACE or event.scancode == KEY_UP:
-			should_jump = true
-	
-	# Ejecutar salto con buffer
-	if should_jump:
-		jump_with_buffer()
-
-func jump_with_buffer():
-	# Input buffering - permite input anticipado
-	input_buffer = true
-	buffer_timer = buffer_time
-	
-	# Salto inmediato
-	bird_up()
-	
-	# Feedback visual adicional (opcional)
-	add_screen_shake()
-
-func add_screen_shake():
-	# Pequeño screen shake para feedback
-	var camera = $Camera2D
-	var original_pos = camera.position
-	
-	# Shake rápido
-	camera.position = original_pos + Vector2(rand_range(-2, 2), rand_range(-2, 2))
-	
-	# Volver a posición original
-	var tween = create_tween()
-	tween.tween_property(camera, "position", original_pos, 0.1)
